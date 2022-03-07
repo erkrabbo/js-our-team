f9b9d0446e03c5d04f6560765349f4d46c59110e
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-07 15:23:42 +0100

    funzionante da migliorare

--- script.js
+++ script.js
@@ -28,37 +28,86 @@ const ourTeam = [
         name : 'Barbara Ramos',
         role : 'Graphic Designer',
         imageUrl : 'img/barbara-ramos-graphic-designer.jpg' 
-    },
+    }
 ]
 
 const teamContainer = document.querySelector('.team-container');
 
+const newMemberNameInput = document.querySelector('#name');
+const newMemberRoleInput = document.querySelector('#role');
+const newMemberImageInput = document.querySelector('#image');
+const addMemberButton = document.querySelector('#addMemberButton');
+
 appendTeamCards();
+addMemberButton.addEventListener('click', addMember);
 
 function appendTeamCards(){
+    teamContainer.innerHTML = '';
     for (let i = 0; i < ourTeam.length; i++){
+        //creo tutti gli elementi che dovrò appendere al container gia esistente
         const teamCard = document.createElement('div');
-        const cardImageContainer = document.createElement('div');
+        const cardImageContainer = document.createElement('div');                       
         const cardImage = document.createElement('img');
         const cardText = document.createElement('div');
 
+        //setto le classi necessarie gia presenti nel css
         teamCard.classList.add('team-card');
-        cardImageContainer.classList.add('card-image');
+        cardImageContainer.classList.add('card-image');                                 
         cardText.classList.add('card-text');
 
-        cardImage.src = ourTeam[i].imageUrl;
+        //estrapolo dall'array esistente i dati necessari e li assegno ai relativi elementi della card
+        cardImage.src = ourTeam[i].imageUrl;                                
         cardImage.alt = ourTeam[i].name;
 
         cardText.innerHTML = `<h3>${ourTeam[i].name}</h3><p>${ourTeam[i].role}</p>`
 
+        //appendo tutti gli elementi
         cardImageContainer.append(cardImage);
 
         teamCard.append(cardImageContainer, cardText);
 
         teamContainer.append(teamCard);
-
-        console.log(ourTeam[i].name, ourTeam[i].role, ourTeam[i].imageUrl);
-
-        
     }
+}
+
+function addMember(){
+    //creo un nuovo oggetto che conterrà le informazioni del nuovo membro
+    const newMember = {};
+
+    //assegno i valori degli input alle proprietà del nuovo oggetto
+    newMember.name = newMemberNameInput.value;
+    newMember.role = newMemberRoleInput.value;
+    newMember.imageUrl = newMemberImageInput.value;
+
+    //inserisco il nuovo oggetto in coda all'array gia esistente
+    ourTeam.push(newMember);
+
+    
+    appendTeamCard();
+}
+
+function appendTeamCard(){
+    //creo tutti gli elementi che dovrò appendere al container gia esistente
+    const teamCard = document.createElement('div');
+    const cardImageContainer = document.createElement('div');                       
+    const cardImage = document.createElement('img');
+    const cardText = document.createElement('div');
+
+    //setto le classi necessarie gia presenti nel css
+    teamCard.classList.add('team-card');
+    cardImageContainer.classList.add('card-image');                                 
+    cardText.classList.add('card-text');
+
+    //estrapolo dall'array esistente i dati necessari e li assegno ai relativi elementi della card
+    cardImage.src = ourTeam[ourTeam.length-1].imageUrl;                                
+    cardImage.alt = ourTeam[ourTeam.length-1].name;
+
+    cardText.innerHTML = `<h3>${ourTeam[ourTeam.length-1].name}</h3><p>${ourTeam[ourTeam.length-1].role}</p>`
+
+    //appendo tutti gli elementi
+    cardImageContainer.append(cardImage);
+
+    teamCard.append(cardImageContainer, cardText);
+
+    teamContainer.append(teamCard);
 }
